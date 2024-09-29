f0a7cde06b035a3133947f41a379f9f41804c2a0
IagoGotoAbreu <iago.abreu@sptech.school>
2024-09-29 15:15:00 -0300

    Herança Part2

---
+++ Guanabara/heranca2/src/main/java/Pessoa.java
@@ -0,0 +1,38 @@
+public abstract class Pessoa {
+    private String nome;
+    private Integer idade;
+    private String sexo;
+
+    public void fazerAniversario(){
+        this.idade++;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{nome: " + nome + ", idade: " + idade + ", sexo: " + sexo + "}";
+    }
+}
---
+++ Guanabara/heranca2/src/main/java/Professor.java
@@ -0,0 +1,24 @@
+public class Professor extends Pessoa {
+    private String especialidade;
+    private Double salario;
+
+    public void receberAumento(Double aumento){
+        this.salario += aumento;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
+}
