151f82402bc1026905fd81e2c9b47d1f49f71a42
IagoGotoAbreu <iago.abreu@sptech.school>
2024-09-29 18:58:06 -0300

    Polimorfismo - Override

---
+++ Guanabara/polimorfismo/src/main/java/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+//        Aminal a = new Animal();
+
+        Mamifero m = new Mamifero();
+        Reptil r = new Reptil();
+        Peixe p = new Peixe();
+        Ave a = new Ave();
+
+        Canguru c = new Canguru();
+        Cachorro k = new Cachorro();
+        Cobra j = new Cobra();
+        Tartaruga t = new Tartaruga();
+        Goldfish g = new Goldfish();
+        Arara e = new Arara();
+
+        c.locomover();
+    }
+}
